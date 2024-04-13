4dfd133a5cd988aee1e0f68a11ebda94d3d494d5
etachott <bfwaend@gmail.com>
2024-04-13 18:59:09 -0300

    Add some routes and persistence

--- src/main/kotlin/ft/etachott/simple_crud/services/MessageService.kt
+++ src/main/kotlin/ft/etachott/simple_crud/services/MessageService.kt
@@ -1,4 +1,27 @@
 package ft.etachott.simple_crud.services
 
-class MessageService {
+import ft.etachott.simple_crud.controllers.Message
+import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.stereotype.Service
+import org.springframework.jdbc.core.query
+import java.util.*
+
+@Service
+class MessageService(val db: JdbcTemplate) {
+    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
+        Message(response.getString("id"), response.getString("text"))
+    }
+
+    fun findMessageById(id: String): List<Message> =
+        db.query("select * from messages where id = ?", id) { response, _ ->
+            Message(response.getString("id"), response.getString("text"))
+        }
+
+    fun save(message: Message) {
+        val id = message.id ?: UUID.randomUUID().toString()
+        db.update(
+            "insert into messages values (?, ?)",
+            id, message.text
+        )
+    }
 }
